8b2956f09e2c011bd97a84e92153f48151cee481
rohit rawat <102246254+rohit27-02@users.noreply.github.com>
2023-07-08 12:05:47 +0530

    random-image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/jsx-no-target-blank */
+import React, { useEffect, useState } from 'react';
+import { FacebookShareButton, TwitterShareButton, WhatsappShareButton, FacebookIcon, TwitterIcon, WhatsappIcon } from 'react-share';
+import axios from 'axios';
+import './App.css'
 
-function App() {
+const RandomImage = ({ imageUrl }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="random-image-container">
+      <h1>Share with friends 💗</h1>
+      <img src={imageUrl} alt="Random" className="random-image" />
+      <div className="share-buttons">
+      <a href="http://www.twitter.com/share" target='_blank'> <TwitterIcon round={true} size={40} /></a>
+        <FacebookShareButton url={imageUrl}>
+          <FacebookIcon round={true} size={40} />
+        </FacebookShareButton>
+        <TwitterShareButton title='random image' url={imageUrl}>
+          <TwitterIcon round={true} size={40} />
+        </TwitterShareButton>
+        <WhatsappShareButton url={imageUrl}>
+          <WhatsappIcon round={true} size={40} />
+        </WhatsappShareButton>
+      </div>
     </div>
   );
-}
+};
+
+const App = () => {
+  const [imageUrl, setImageUrl] = useState('');
+  const api = process.env.REACT_APP_UNSPLASH_API_KEY;
+
+  useEffect(() => {
+    const fetchRandomImage = async () => {
+      try {
+        const response = await axios.get('https://api.unsplash.com/photos/random', {
+          params: {
+            client_id: api,
+            orientation: "squarish",
+          },
+        });
+        const imageUrl = response.data.urls.regular;
+        setImageUrl(imageUrl);
+        
+
+      } catch (error) {
+        console.error('Error fetching random image:', error);
+      }
+    };
+
+    fetchRandomImage();
+  }, []);
+
+  return (
+    <div className="app">
+      {imageUrl && <RandomImage imageUrl={imageUrl} />}
+    </div>
+  );
+};
 
 export default App;
